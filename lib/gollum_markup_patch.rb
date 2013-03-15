7fc1737c7a9215a8dce9e2959e82dc08546c1e69
David Rios <david.rios.gomes@gmail.com>
2013-03-15 14:21:04 -0300

    changed absent link

--- lib/gollum_markup_patch.rb
+++ lib/gollum_markup_patch.rb
@@ -36,5 +36,10 @@ module Gollum
       toc = toc.to_xml(@to_xml) if toc != nil
       [doc, toc]
     end
+
+    old_process_page_link_tag = instance_method(:process_page_link_tag)
+    define_method(:process_page_link_tag) do |tag|
+      old_process_page_link_tag.bind(self).(tag).sub('"internal absent"', '"internal new"')
+    end
   end
 end
